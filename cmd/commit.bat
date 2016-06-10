ac657b9ec12bd5a306ad334a99fe5e5d3c54548d
wukejiong <724260080@qq.com>
2016-06-10 10:48:42 +0800

    123

---
+++ cmd/commit.bat
@@ -0,0 +1,9 @@
+cd ../
+git config --global push.default simple
+git config --global user.email 724260080@qq.com
+git config --global user.name wukejiong
+git pull
+git add -A
+git commit -m "123"
+git push
+pause
